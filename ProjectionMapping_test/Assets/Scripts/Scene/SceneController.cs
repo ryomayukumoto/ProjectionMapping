51b55040ba12576debf6d65c3f2ee7b627e9ab47
ryomayukumoto <ryomayukkun@gmail.com>
2023-01-19 10:44:53 +0900

    シーン転移と動画流せるシーン追加

--- ProjectionMapping_test/Assets/Scripts/Scene/SceneController.cs
+++ ProjectionMapping_test/Assets/Scripts/Scene/SceneController.cs
@@ -8,25 +8,35 @@ public class SceneController : BaseScene
 
     public static ShowSceneController ShowSceneCtrl;
     public static MappingSceneController MapSceneCtrl;
+    public static DefaultSceneController defaultSceneCtrl;
 
     void Start()
     {
         ShowSceneCtrl = GameObject.FindWithTag("SceneManager").GetComponent<ShowSceneController>();
         MapSceneCtrl = GameObject.FindWithTag("SceneManager").GetComponent<MappingSceneController>();
+        defaultSceneCtrl = GameObject.FindWithTag("SceneManager").GetComponent<DefaultSceneController>();
+        
     }
 
     void Update()
     {
+       
         switch(ScenePhase)
         {
-            case Scene.MappingScene:
+            case BaseScene.Scene.ShowScene:
+
+                ShowSceneCtrl.DoUpdate();
+                break;
+            case BaseScene.Scene.MappingScene:
                 
                 break;
             
-            case Scene.ShowScene:
-                break;
+            
 
-            case Scene.DefaultScene:
+            case BaseScene.Scene.DefaultScene:
+                //Debug.Log("d");
+                if(!defaultSceneCtrl.IsCheck)
+                    defaultSceneCtrl.CheckNextScene();
                 break;
 
             default:
